9876f50c7edd342003944d20a57d7642929d2194
Loreley <mail@lory.dev>
2021-11-14 13:15:23 +0100

    moved dmxprojects folder to user appdir
    Signed-off-by: Loreley <mail@lory.dev>

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,24 @@
+import appdirs
 from usbdmx import usbdmx
 from flask import Flask, jsonify, request
-import os
 import json
 import logging
 from pathlib import Path
-#from flask_cors import CORS, cross_origin
 from flask_cors import CORS
 from flask_socketio import SocketIO
+from appdirs import *
 
+# get AppData directory based on OS
+appdata = appdirs.user_data_dir("lyra", "Loreley")
+
+# setup Flask server
 app = Flask(__name__)
-#app.config["SECRET_KEY"] = "secret!"
 logging.getLogger('flask_cors').level = logging.DEBUG
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-#interfaces: list[usbdmx.FX5Interface] = []
-#opened_interface: usbdmx.FX5Interface
-
-
+# WebSocket handler class
+# Handles incoming websocket connections and redirects them to the connected DMX interface
 class DMXWSHandler():
     def __init__(self):
         self.opened_interface: usbdmx.FX5Interface = None
@@ -29,7 +30,6 @@ class DMXWSHandler():
     def on_open_interface(self, message):
         interface_id = message["interface"]
         index = [ x.serial.decode("utf-8") for x in self.interfaces ].index(interface_id)
-        print(index)
         if index != -1:
             self.opened_interface: usbdmx.FX5Interface = interfaces[index]
             self.opened_interface.open()
@@ -38,10 +38,9 @@ class DMXWSHandler():
     def on_interface_message(self, message):
         address = message["address"]
         value = message["value"]
-        print(value)
         self.opened_interface.set_dmx(value, address)
 
-
+# returns a list of connected DMX interfaces
 @app.route("/interfaces/find")
 def findInterfaces():
     global interfaces
@@ -52,15 +51,16 @@ def findInterfaces():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+# fetches the content of the db.json file and returns the project data
 @app.route("/projects")
 def getAllProjects():
     projects = []
     try:
-        with open("%s/dmxprojects/db.json" % os.getcwd(), "r") as maindb:
+        with open("%s/dmxprojects/db.json" % appdata, "r") as maindb:
             maindb_data = json.load(maindb)
             for project_id in maindb_data:
                 try:
-                    with open(f"{os.getcwd()}/dmxprojects/{project_id}.json", "r") as project_file:
+                    with open(f"{appdata}/dmxprojects/{project_id}.json", "r") as project_file:
                         project_data = json.load(project_file)
                         project_name: str = project_data["name"]
                         project_uid: str = project_data["uid"]
@@ -81,10 +81,11 @@ def getAllProjects():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+# get a specific project
 @app.route("/projects/<string:project_id>", methods=["GET"])
 def getProject(project_id):
     try:
-        with open(f"{os.getcwd()}/dmxprojects/{project_id}.json", "r") as project_file:
+        with open(f"{appdata}/dmxprojects/{project_id}.json", "r") as project_file:
             project_data = json.load(project_file)
             response = jsonify(project_data)
             response.headers.add('Access-Control-Allow-Origin', '*')
@@ -94,34 +95,40 @@ def getProject(project_id):
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
 
+# update a project
 @app.route("/projects/<string:project_id>", methods=["POST"])
 def updateProject(project_id):
     if not request.json:
         response = jsonify({"code": "100"})
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
-    with open(f"{os.getcwd()}/dmxprojects/db.json", "r") as maindb:
+    with open(f"{appdata}/dmxprojects/db.json", "r") as maindb:
         maindb_data: list[str] = json.load(maindb)
         if project_id not in maindb_data:
             maindb_data.append(project_id)
-            with open(f"{os.getcwd()}/dmxprojects/db.json", "w") as outfile:
+            with open(f"{appdata}/dmxprojects/db.json", "w") as outfile:
                 json.dump(maindb_data, outfile)
-        with open(f"{os.getcwd()}/dmxprojects/{project_id}.json", "w") as outfile:
+        with open(f"{appdata}/dmxprojects/{project_id}.json", "w") as outfile:
             json.dump(request.json, outfile)
             response = jsonify({"code": "200"})
             response.headers.add('Access-Control-Allow-Origin', '*')
             return response
 
+# initialize API
 def _init_API():
+    # check if appdir folder exists
+    appdir_dir = Path("%s" % appdata)
+    if not appdir_dir.exists():
+        os.mkdir("%s" % appdata)
     # check if project folder exists
-    dmx_dir = Path("%s/dmxprojects/" % os.getcwd())
+    dmx_dir = Path("%s/dmxprojects/" % appdata)
     if not dmx_dir.exists():
-        os.mkdir("%s/dmxprojects" % os.getcwd())
+        os.mkdir("%s/dmxprojects" % appdata)
     # check if main db exists
-    main_db_path = Path("%s/dmxprojects/db.json" % os.getcwd())
+    main_db_path = Path("%s/dmxprojects/db.json" % appdata)
     if not main_db_path.exists():
         # create main db
-        with open("%s/dmxprojects/db.json" % os.getcwd(), "w") as outfile:
+        with open("%s/dmxprojects/db.json" % appdata, "w") as outfile:
             json.dump([], outfile)
 
 def _getInterfaceId(interface: usbdmx.FX5Interface) -> str:
@@ -131,42 +138,13 @@ def _getInterfaceId(interface: usbdmx.FX5Interface) -> str:
 def on_connection():
     print('client connected connected')
 
-#@socketio.on("open_interface")
-#def ws_open_interface(message):
- #   interface_id = message["interface"]
- #   global interfaces
- #   index = [ x.serial.decode("utf-8") for x in interfaces ].index(interface_id)
- #   print(index)
- #   if index != -1:
- #       global opened_interface
- #       opened_interface = interfaces[index]
-#        opened_interface.open()
-#        opened_interface.mode(6)
-        
-
-
-#@socketio.on("interface_message")
-#def ws_event(message):
-#    address = message["address"]
-#    value = message["value"]
-#    print(value)
-#    global opened_interface
-#    opened_interface.open()
-#    opened_interface.mode(6)
-#    opened_interface.set_dmx(value, address)
-
 def main_http():
     _init_API()
     app.run(debug=True, port=3832)
 
 
-#def close_interface():
-    #global opened_interface
-    #opened_interface.close()
-
-
 if __name__ == "__main__":
-    #atexit.register(close_interface)
+    _init_API()
     dmxWSHandler = DMXWSHandler()
     
     socketio.on("connect")(dmxWSHandler.on_connection)
